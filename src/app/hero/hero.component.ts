586a56639b9abaa7890d0f4ef73d7a18912700bd
roycabrera <roycabrera80@gmail.com>
2019-02-28 21:46:45 -0500

    Estilos con Bulma css

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HerosService } from "../services/heros.service";
 import { Hero } from "../models/hero";
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-hero',
@@ -17,6 +18,17 @@ export class HeroComponent implements OnInit {
    }
 
   ngOnInit() {
+    $(document).ready(function() {
+
+// Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+
+      });
+    });
     this.getAllHeros();
   }
 
